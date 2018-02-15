901868242793701968a088e71539ca35dae0edcf
Ariel Burriss <ariburr@gmail.com>
2018-02-15 12:09:05 -0700

    set up moongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -7,3 +11,5 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+console.log(`Server is listening on port: ${PORT}`);
+console.log('Visit:', `http://localhost:${PORT}`);
