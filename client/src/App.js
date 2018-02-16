74dbbce20c0bfb0182ea7ee1702da01a9e00d43f
Ariel Burriss <ariburr@gmail.com>
2018-02-15 17:54:45 -0700

    creat react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Your Emaily</h2>
+        <a href='http://localhost:5000/auth/google'>Sign in with Google</a>
+      </div>
+    );
+  }
+}
+
+export default App;
