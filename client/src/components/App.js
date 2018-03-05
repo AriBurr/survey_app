b6470efb5636562b1f836b196e75e9a9a3c09c18
Ariel Burriss <ariburr@gmail.com>
2018-03-05 14:23:23 -0700

    logout redirect in header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import * as actions from '../actions';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="container">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route exact path="/surveys/new" component={SurveyNew} />
-      </Switch>
-    </div>
-  );
-};
+const Dashboard = () => <h2>Dashboard</h2>
+const Landing = () => <h2>Landing</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
 
-export default App;
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  renderContent = () => {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <a href="/auth/google">Login With Google</a>;
+      default:
+        return <a href="/api/logout">Logout</a>;
+    }
+  };
+
   render() {
     return (
       <nav>
-        <div className='nav-wrapper'>
-          <a className='left brand-logo'>
-          </a>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li>{this.renderContent()}</li>
+          </ul>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
